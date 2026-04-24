e6798dc2426461e4f97723a5c64109d877305bfc
LucasISkolan <156677949+LucasISkolan@users.noreply.github.com>
2026-04-24 13:01:58 +0200

    Bug fixes in ASTAllocator

--- include/Frontend/ASTAllocator.hpp
+++ include/Frontend/ASTAllocator.hpp
@@ -2,6 +2,7 @@
 
 #include <type_traits>
 
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/Support/Allocator.h"
 
 #include "include/AST/Type.hpp"
@@ -11,32 +12,39 @@ namespace c {
 class ASTAllocator {
 private:
     llvm::BumpPtrAllocator Allocator;
+
+    llvm::DenseMap<BuiltinType::BuiltinKind, BuiltinType*> Builtins;
 public:
     ASTAllocator() = default;
     ~ASTAllocator() = default;
 
-    BuiltinType* getBuiltinType(BuiltinType::BuiltinKind kind) {
-        
+    void createBuiltinTypes() {
+        uint8_t amnt_builtins = BuiltinType::getAmntBuiltinTypes();
+        Builtins.grow(amnt_builtins);
+        for (uint8_t k = 0; k < amnt_builtins; k++) {
+            auto kind = static_cast<BuiltinType::BuiltinKind>(k);
+            Builtins.insert({kind, new BuiltinType(kind)}); // FIXME: I don't like "new"
+        }
     }
 
-    void* Allocate(size_t amnt, size_t size) {
+    const BuiltinType* getBuiltinType(BuiltinType::BuiltinKind kind) {
+        assert(!Builtins.empty() && "Builtins were never created. Call ASTAllocator::createBuiltinTypes()");
+        return Builtins.lookup(kind);
+    }
 
+    void* Allocate(size_t amnt, size_t align) {
+        return Allocator.Allocate(amnt, align);
     }
 
     template <typename T>
     T* Allocate(size_t amnt) {
-        return static_cast<T*>(Allocate(amnt, sizeof(T)));
+        return Allocator.Allocate(amnt, sizeof(T));
     }
 
     template <typename T, typename... Args>
     T* Create(Args&&... args) {
-        T init = T(std::forward<Args>(args)...);
-        return Create<T>(std::move(init));
-    }
-
-    template <typename T>
-    T* Create(T&& init) {
-
+        T* mem = (T*)Allocator.Allocate(sizeof(T), alignof(T));
+        return new (mem) T(std::forward<Args>(args)...);
     }
 };
 
