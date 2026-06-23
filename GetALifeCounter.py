15a946a2c36d7b4e6d90f3daa2a1e689ef20e54d
Överbefälhavaren <166145221+overbefalhavaren@users.noreply.github.com>
2026-06-23 21:17:33 +0200

    Updated getALifeCounter to also show bash linecount

--- GetALifeCounter.py
+++ GetALifeCounter.py
@@ -3,6 +3,7 @@ import os
 cxx_linecount: int = 0
 cmake_linecount: int = 0
 python_linecount: int = 0
+bash_linecount: int = 0
 
 def find_base_directory() -> str:
     return os.path.dirname(__file__)
@@ -10,17 +11,19 @@ def find_base_directory() -> str:
 def count_lines(path: str) -> int:
     with open(path, 'r') as f:
         return sum(1 for _ in f)
-    
-def recurse_count_dir(dir: str) -> None:
-    global cxx_linecount, cmake_linecount, python_linecount
+
+def count_dir(dir: str, count_recurse: bool) -> None:
+    global cxx_linecount, cmake_linecount, python_linecount, bash_linecount
 
     for file in os.listdir(dir):
-        if os.path.isdir(os.path.join(dir, file)):
-            recurse_count_dir(os.path.join(dir, file))
-        elif file.endswith(".hpp") or file.endswith("cpp"):
+        if count_recurse and os.path.isdir(os.path.join(dir, file)):
+            count_dir(os.path.join(dir, file), True)
+        elif file.endswith(".hpp") or file.endswith(".cpp"):
             cxx_linecount += count_lines(os.path.join(dir, file))
         elif file.endswith(".py"):
             python_linecount += count_lines(os.path.join(dir, file))
+        elif file.endswith(".sh"):
+            bash_linecount += count_lines(os.path.join(dir, file))
         elif file == "CmakeLists.txt":
             cmake_linecount += count_lines(os.path.join(dir, file))
 
@@ -28,19 +31,15 @@ def main() -> None:
     global cxx_linecount, cmake_linecount, python_linecount
 
     base: str = find_base_directory()
-    for file in os.listdir(base):
-        if file.endswith(".py"):
-            python_linecount += count_lines(os.path.join(base, file))
-        elif file == "CmakeLists.txt":
-            cmake_linecount += count_lines(os.path.join(base, file))
-    
-    recurse_count_dir(os.path.join(base, "include"))
-    recurse_count_dir(os.path.join(base, "src"))
+    count_dir(base, False)
+    count_dir(os.path.join(base, "include"), True)
+    count_dir(os.path.join(base, "src"), True)
 
     print("Project line count, not including tests.")
     print(f"Lines of C++: {cxx_linecount}")
     print(f"Lines of CMake: {cmake_linecount}")
     print(f"Lines of Python: {python_linecount}")
+    print(f"Lines of Bash: {bash_linecount}")
     print("Dude you need to get a life.")
 
 if __name__ == "__main__":
